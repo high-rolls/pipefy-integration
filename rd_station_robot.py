3a9f514e6c5d7c7ec33d41453474e3032d3e9147
Paulo V F G Pivotto <paulo_vitor652@hotmail.com>
2022-04-14 11:33:44 -0300

    headless mode

--- rd_station_robot.py
+++ rd_station_robot.py
@@ -1,42 +1,46 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import re
 import time
+import os
 
 
 def upload_deal_files(deal, files=None):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 10)
     #driver.implicitly_wait(10) # usar espera implicita em todos os comandos
     driver.get(f"https://plugcrm.net/app#/deals/{deal}")
-    
-    email_input = driver.find_element(By.ID, "email")
-    email_input.send_keys("dev@seguralta.com.br")
-    
-    password_input = driver.find_element(By.ID, "password")
-    password_input.send_keys("EDcP9Mrt2RxnAQ")
-    
-    submit_button = driver.find_element(By.CSS_SELECTOR, "button[type=submit]")
-    submit_button.click()
 
-    link_instancia = wait.until(lambda d: d.find_element(By.LINK_TEXT, "Seguralta Corretora de Seguros"))
-    link_instancia.click()
+    # login page
+    driver.find_element(By.ID, "email").send_keys("dev@seguralta.com.br")
+    driver.find_element(By.ID, "password").send_keys("EDcP9Mrt2RxnAQ")
+    driver.find_element(By.CSS_SELECTOR, "button[type=submit]").click()
+
+    wait.until(lambda d: d.find_element(By.LINK_TEXT, "Seguralta Corretora de Seguros")).click()
     
-    close_dialog_button = driver.find_element(By.CSS_SELECTOR, "body > div.plug-crm-app > div.md-dialog-container > md-dialog > div > button")
-    if close_dialog_button:
-        close_dialog_button.click()
+    dialog_close_button_By = (By.CSS_SELECTOR, "md-dialog .dialog-header button")
+    if driver.find_elements(*dialog_close_button_By):
+        driver.find_element(*dialog_close_button_By).click()
     
-    aba_arquivos = driver.find_element(By.CSS_SELECTOR, "md-tab-item:nth-child(6)")
+    time.sleep(1)
+    aba_arquivos = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "md-tab-item:nth-child(6)")))
     aba_arquivos.click()
     
-    file_upload_input = driver.find_element(By.CSS_SELECTOR, "#upload-file-input > input[type=file]")
-    files_string = "\n".join(files)
-    file_upload_input.send_keys(files_string)
+    driver.find_element(By.CSS_SELECTOR, "#upload-file-input > input[type=file]").send_keys("\n".join(files))
+    wait.until(lambda d: "Uploading..." not in driver.find_element(By.CSS_SELECTOR, "#deal_container table").text)
 
-    file_table = driver.find_element(By.CSS_SELECTOR, "#deal_container > div > div.layout-padding.flex > div:nth-child(2) > md-card > md-table-container > table")
-    wait.until(lambda d: "Uploading..." not in file_table.text)
     driver.close()
+
+
+def create_opportunity():
+    api_token = os.getenv("RD_STATION_API_TOKEN")
+
 
 if __name__ == "__main__":
     upload_deal_files("6254368e507374000f9fac61", [
